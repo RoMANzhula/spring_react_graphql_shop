d2e3f99353982858713231ad810d948bcf349eda
RoMANzhula <roman1985al@ukr.net>
2024-05-28 22:34:24 +0300

    Add user model

---
+++ src/main/java/org/romanzhula/spring_react_graphql_grocery_store/models/User.java
@@ -0,0 +1,149 @@
+package org.romanzhula.spring_react_graphql_grocery_store.models;
+
+import jakarta.persistence.*;
+import org.romanzhula.spring_react_graphql_grocery_store.models.enums.Role;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+
+    /**
+     * type User {
+     *     id: ID!
+     *     firstName: String!
+     *     lastName: String!
+     *     email: String!
+     *     phoneNumber: String!
+     *     address: String!
+     *     city: String!
+     *     state: String!
+     *     zipCode: String!
+     *     roles: [Role]!
+     *     password: String!
+     * }
+     * */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
+    private Long id;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PHONE_NUMBER")
+    private String phoneNumber;
+
+    @Column(name = "ADDRESS")
+    private String address;
+
+    @Column(name = "CITY")
+    private String city;
+
+    @Column(name = "STATE")
+    private String state;
+
+    @Column(name = "ZIPCODE")
+    private String zipCode;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "ROLE")
+    private Role role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
