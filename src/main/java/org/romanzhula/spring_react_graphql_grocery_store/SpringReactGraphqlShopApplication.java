cb15dea91b6e0d3531f9aef04c23bb7b28f810d7
RoMANzhula <roman1985al@ukr.net>
2024-05-28 20:30:54 +0300

    Initial commit

---
+++ src/main/java/org/romanzhula/spring_react_graphql_grocery_store/SpringReactGraphqlShopApplication.java
@@ -0,0 +1,13 @@
+package org.romanzhula.spring_react_graphql_grocery_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactGraphqlShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactGraphqlShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/romanzhula/spring_react_graphql_grocery_store/SpringReactGraphqlShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.romanzhula.spring_react_graphql_grocery_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactGraphqlShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
