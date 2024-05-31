f1eb4a8266e1c909afae07ec876cbcbf21111c83
RoMANzhula <roman1985al@ukr.net>
2024-05-31 23:37:54 +0300

    Add UserDetailsImpl with implements UserDetails methods

---
+++ src/main/java/org/romanzhula/spring_react_graphql_grocery_store/configurations/security/implementations/UserDetailsImpl.java
@@ -0,0 +1,72 @@
+package org.romanzhula.spring_react_graphql_grocery_store.configurations.security.implementations;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.romanzhula.spring_react_graphql_grocery_store.models.User;
+import org.romanzhula.spring_react_graphql_grocery_store.models.enums.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    private Role role;
+
+    public UserDetailsImpl(String email, String password, Role role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public static UserDetailsImpl build(User user) {
+        return new UserDetailsImpl(
+                user.getEmail(),
+                user.getPassword(),
+                user.getRole()
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;  // Using email as the username
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
