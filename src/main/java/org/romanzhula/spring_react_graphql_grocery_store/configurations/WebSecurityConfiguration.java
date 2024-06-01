f8ceeaf11b0090dadb0e75b76c1e74bbd9bece10
RoMANzhula <roman1985al@ukr.net>
2024-06-01 19:19:14 +0300

    Add AuthEntryPointJwt

--- src/main/java/org/romanzhula/spring_react_graphql_grocery_store/configurations/WebSecurityConfiguration.java
+++ src/main/java/org/romanzhula/spring_react_graphql_grocery_store/configurations/WebSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package org.romanzhula.spring_react_graphql_grocery_store.configurations;
 
 import org.romanzhula.spring_react_graphql_grocery_store.configurations.security.implementations.UserDetailServiceImpl;
+import org.romanzhula.spring_react_graphql_grocery_store.configurations.security.jwt.components.AuthEntryPointJwt;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
