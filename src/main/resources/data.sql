1a9799874341fd31de9fe22849816f95a70e686f
RoMANzhula <roman1985al@ukr.net>
2024-06-02 19:37:23 +0300

    Change password on ByCrypt admin - 111 seller - 222 customer - 333

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,35 +1,35 @@
 
 INSERT INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD, ROLE) VALUES
-('Admin', 'Admin', 'admin@example.com', '123-456-7890', '123 Admin St', 'AdminCity', 'AC', '12345', '111', 'ROLE_ADMIN');
+('Admin', 'Admin', 'admin@example.com', '123-456-7890', '123 Admin St', 'AdminCity', 'AC', '12345', '$2y$05$ceTzfRO2M9m4..NEuRa1HekfGA6885BHELlZ.dfycaQpxaywDqJpy', 'ROLE_ADMIN');
 
 INSERT INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD, ROLE) VALUES
-('Seller1', 'Seller1', 'seller1@example.com', '111-111-1111', '456 Seller St', 'SellerCity', 'SC', '54321', '222', 'ROLE_SELLER');
+('Seller1', 'Seller1', 'seller1@example.com', '111-111-1111', '456 Seller St', 'SellerCity', 'SC', '54321', '$2y$05$DBt9W3kaey8jMQO14/hQVuIDb1q4l5cEpCqpL5ePN9GfVrJxvgHqS', 'ROLE_SELLER');
 INSERT INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD, ROLE) VALUES
-('Seller2', 'Seller2', 'seller2@example.com', '222-222-2222', '457 Seller St', 'SellerCity', 'SC', '12345', '222', 'ROLE_SELLER');
+('Seller2', 'Seller2', 'seller2@example.com', '222-222-2222', '457 Seller St', 'SellerCity', 'SC', '12345', '$2y$05$DBt9W3kaey8jMQO14/hQVuIDb1q4l5cEpCqpL5ePN9GfVrJxvgHqS', 'ROLE_SELLER');
 INSERT INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD, ROLE) VALUES
-('Seller3', 'Seller3', 'seller3@example.com', '333-333-3333', '458 Seller St', 'SellerCity', 'SC', '14789', '222', 'ROLE_SELLER');
+('Seller3', 'Seller3', 'seller3@example.com', '333-333-3333', '458 Seller St', 'SellerCity', 'SC', '14789', '$2y$05$DBt9W3kaey8jMQO14/hQVuIDb1q4l5cEpCqpL5ePN9GfVrJxvgHqS', 'ROLE_SELLER');
 INSERT INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD, ROLE) VALUES
-('Seller4', 'Seller4', 'seller4@example.com', '444-444-4444', '459 Seller St', 'SellerCity', 'SC', '67774', '222', 'ROLE_SELLER');
+('Seller4', 'Seller4', 'seller4@example.com', '444-444-4444', '459 Seller St', 'SellerCity', 'SC', '67774', '$2y$05$DBt9W3kaey8jMQO14/hQVuIDb1q4l5cEpCqpL5ePN9GfVrJxvgHqS', 'ROLE_SELLER');
 INSERT INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD, ROLE) VALUES
-('Seller5', 'Seller5', 'seller5@example.com', '555-555-5555', '460 Seller St', 'SellerCity', 'SC', '20987', '222', 'ROLE_SELLER');
+('Seller5', 'Seller5', 'seller5@example.com', '555-555-5555', '460 Seller St', 'SellerCity', 'SC', '20987', '$2y$05$DBt9W3kaey8jMQO14/hQVuIDb1q4l5cEpCqpL5ePN9GfVrJxvgHqS', 'ROLE_SELLER');
 
 INSERT INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD, ROLE) VALUES
-('Customer1', 'Customer1', 'customer1@example.com', '666-666-6666', '123 Customer St', 'CustomerCity', 'CC', '65432', '333', 'ROLE_CUSTOMER');
+('Customer1', 'Customer1', 'customer1@example.com', '666-666-6666', '123 Customer St', 'CustomerCity', 'CC', '65432', '$2y$05$FBaeQoucw2sRe45zPUglIOygH0EiRKxj/mksjRrigzPPMdnDs.RaS', 'ROLE_CUSTOMER');
 INSERT INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD, ROLE) VALUES
-('Customer2', 'Customer2', 'customer2@example.com', '777-777-7777', '124 Customer St', 'CustomerCity', 'CC', '56745', '333', 'ROLE_CUSTOMER');
+('Customer2', 'Customer2', 'customer2@example.com', '777-777-7777', '124 Customer St', 'CustomerCity', 'CC', '56745', '$2y$05$FBaeQoucw2sRe45zPUglIOygH0EiRKxj/mksjRrigzPPMdnDs.RaS', 'ROLE_CUSTOMER');
 INSERT INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD, ROLE) VALUES
-('Customer3', 'Customer3', 'customer3@example.com', '888-888-8888', '125 Customer St', 'CustomerCity', 'CC', '75634', '333', 'ROLE_CUSTOMER');
+('Customer3', 'Customer3', 'customer3@example.com', '888-888-8888', '125 Customer St', 'CustomerCity', 'CC', '75634', '$2y$05$FBaeQoucw2sRe45zPUglIOygH0EiRKxj/mksjRrigzPPMdnDs.RaS', 'ROLE_CUSTOMER');
 INSERT INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD, ROLE) VALUES
-('Customer4', 'Customer4', 'customer4@example.com', '999-999-9999', '126 Customer St', 'CustomerCity', 'CC', '45890', '333', 'ROLE_CUSTOMER');
+('Customer4', 'Customer4', 'customer4@example.com', '999-999-9999', '126 Customer St', 'CustomerCity', 'CC', '45890', '$2y$05$FBaeQoucw2sRe45zPUglIOygH0EiRKxj/mksjRrigzPPMdnDs.RaS', 'ROLE_CUSTOMER');
 INSERT INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD, ROLE) VALUES
-('Customer5', 'Customer5', 'customer5@example.com', '101-010-1010', '127 Customer St', 'CustomerCity', 'CC', '16735', '333', 'ROLE_CUSTOMER');
+('Customer5', 'Customer5', 'customer5@example.com', '101-010-1010', '127 Customer St', 'CustomerCity', 'CC', '16735', '$2y$05$FBaeQoucw2sRe45zPUglIOygH0EiRKxj/mksjRrigzPPMdnDs.RaS', 'ROLE_CUSTOMER');
 INSERT INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD, ROLE) VALUES
-('Customer6', 'Customer6', 'customer6@example.com', '102-020-2020', '128 Customer St', 'CustomerCity', 'CC', '43688', '333', 'ROLE_CUSTOMER');
+('Customer6', 'Customer6', 'customer6@example.com', '102-020-2020', '128 Customer St', 'CustomerCity', 'CC', '43688', '$2y$05$FBaeQoucw2sRe45zPUglIOygH0EiRKxj/mksjRrigzPPMdnDs.RaS', 'ROLE_CUSTOMER');
 INSERT INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD, ROLE) VALUES
-('Customer7', 'Customer7', 'customer7@example.com', '103-030-3030', '129 Customer St', 'CustomerCity', 'CC', '87456', '333', 'ROLE_CUSTOMER');
+('Customer7', 'Customer7', 'customer7@example.com', '103-030-3030', '129 Customer St', 'CustomerCity', 'CC', '87456', '$2y$05$FBaeQoucw2sRe45zPUglIOygH0EiRKxj/mksjRrigzPPMdnDs.RaS', 'ROLE_CUSTOMER');
 INSERT INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD, ROLE) VALUES
-('Customer8', 'Customer8', 'customer8@example.com', '104-040-4040', '130 Customer St', 'CustomerCity', 'CC', '34678', '333', 'ROLE_CUSTOMER');
+('Customer8', 'Customer8', 'customer8@example.com', '104-040-4040', '130 Customer St', 'CustomerCity', 'CC', '34678', '$2y$05$FBaeQoucw2sRe45zPUglIOygH0EiRKxj/mksjRrigzPPMdnDs.RaS', 'ROLE_CUSTOMER');
 INSERT INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD, ROLE) VALUES
-('Customer9', 'Customer9', 'customer9@example.com', '105-050-5050', '131 Customer St', 'CustomerCity', 'CC', '65432', '333', 'ROLE_CUSTOMER');
+('Customer9', 'Customer9', 'customer9@example.com', '105-050-5050', '131 Customer St', 'CustomerCity', 'CC', '65432', '$2y$05$FBaeQoucw2sRe45zPUglIOygH0EiRKxj/mksjRrigzPPMdnDs.RaS', 'ROLE_CUSTOMER');
 INSERT INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, ADDRESS, CITY, STATE, ZIPCODE, PASSWORD, ROLE) VALUES
-('Customer10', 'Customer10', 'customer10@example.com', '106-060-6060', '132 Customer St', 'CustomerCity', 'CC', '45098', '333', 'ROLE_CUSTOMER');
+('Customer10', 'Customer10', 'customer10@example.com', '106-060-6060', '132 Customer St', 'CustomerCity', 'CC', '45098', '$2y$05$FBaeQoucw2sRe45zPUglIOygH0EiRKxj/mksjRrigzPPMdnDs.RaS', 'ROLE_CUSTOMER');
