d550e9277b44bc904f640ba435162dfe5127f0cc
RoMANzhula <roman1985al@ukr.net>
2024-06-04 21:23:20 +0300

    Add homePage.js

--- src/main/frontend/graphql-shop/src/index.js
+++ src/main/frontend/graphql-shop/src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import client from './apolloClient';
+import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/authContext';
+
+// Our React app needs access to:
+// Client
+// Authorization Context
+// Browser Router (React Router) /register /login /...
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthProvider>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </BrowserRouter>
+    </ApolloProvider>
+  </AuthProvider>
 );
 
---
+++ src/main/frontend/graphql-shop/src/pages/homePage.js
@@ -0,0 +1,9 @@
+
+
+function Homepage() {
+  return (
+    <h2>It is our HomePage</h2>
+  )
+}
+
+export default Homepage;
