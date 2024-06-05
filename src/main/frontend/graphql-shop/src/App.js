17630b66d08072fa0572f04a6c44bc1fd013a2cc
RoMANzhula <roman1985al@ukr.net>
2024-06-05 14:36:22 +0300

    Add graphql-tag

--- src/main/frontend/graphql-shop/src/App.js
+++ src/main/frontend/graphql-shop/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Homepage from './pages/homePage';
+import Homepage from './pages/home';
 import Navbar from './components/navbar';
 
 
--- src/main/frontend/graphql-shop/src/pages/homePage.js
+++
@@ -1,9 +0,0 @@
-
-
-function Homepage() {
-  return (
-    <h2>It is our HomePage</h2>
-  )
-}
-
-export default Homepage;
