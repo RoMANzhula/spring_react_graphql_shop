1393d64c92e9dcc24f9f9a23d7b71c3bd723fe26
RoMANzhula <roman1985al@ukr.net>
2024-06-05 14:16:18 +0300

    Add links login and register to navbar

--- src/main/frontend/graphql-shop/src/components/navbar.js
+++ src/main/frontend/graphql-shop/src/components/navbar.js
@@ -8,8 +8,12 @@ function Navbar() {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h5" component="div">
-            <Link to="/">ReactLogin</Link>
+            <Link to="/" style={{textDecoration: "none", color: "black"}}>Greeting</Link>
           </Typography>
+          <Box alignItems="right" sx={{flexGrow: 1, textAlign: "right"}}>
+          <Link to="/login" style={{textDecoration: "none", color: "white", margin: "17px"}}>Login</Link>
+          <Link to="/register" style={{textDecoration: "none", color: "black"}}>Register</Link>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
